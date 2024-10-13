5efa63cc6d5ab865ad3789d19d4165eeaac8e6b5
sergey ihanov <uhanov.sergio@gmail.com>
2024-10-13 14:54:10 +0300

    correction bugs

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+import style from '@/styles/page/error.module.css'
 export default function Error() { 
-	return <div>Error Page</div>;
+	return <h1 className={style.error__title}>Треба зареєструватися або увійти =(</h1>;
 }
